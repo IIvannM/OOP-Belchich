0ab2c945b6c5bf6f9f77c7972d18aff0ed52cc60
IIvannM <93133422+IIvannM@users.noreply.github.com>
2023-03-28 13:46:59 +0300

    Adding Lab9

--- WinForms9/WinForms9/Form1.Designer.cs
+++ WinForms9/WinForms9/Form1.Designer.cs
@@ -37,7 +37,9 @@
             this.ClientSize = new System.Drawing.Size(851, 630);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove_1);
             this.ResumeLayout(false);
 
         }
--- WinForms9/WinForms9/Form1.cs
+++ WinForms9/WinForms9/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinForms9
 {
@@ -17,28 +18,37 @@ namespace WinForms9
         {
             InitializeComponent();
         }
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+
+            SolidBrush bgEllipseBrash = new SolidBrush(ColorTranslator.FromHtml("#65C694"));
+            g.FillEllipse(bgEllipseBrash,-450, 150, 1500, 500);
+
+            SolidBrush bgEllipseBrash2 = new SolidBrush(ColorTranslator.FromHtml("#55AD80"));
+            g.FillEllipse(bgEllipseBrash2, -450, 350, 1500, 500);
+
+
             SolidBrush blackBrush = new SolidBrush(Color.Black);
             g.FillRectangle(blackBrush, 60, 70, 120, 120);
 
             SolidBrush brushLY = new SolidBrush(Color.LightYellow);
             g.FillRectangle(brushLY, 100, 110, 40, 40);
 
+            SolidBrush brushGreen = new SolidBrush(Color.Green);
+            g.FillRectangle(brushLY, 100, 110, 40, 40);
+
             SolidBrush brownBrush = new SolidBrush(Color.Brown);
             g.FillRectangle(brownBrush, 300, 300, 20, 30);
 
             SolidBrush brushG = new SolidBrush(Color.Green);
-            g.FillEllipse(brushG, 230, 75, 40, 40);
-
+          
             Pen f = new Pen(Color.Red, 3);
             f.DashStyle = DashStyle.Dash;
-            g.DrawEllipse(f, 300, 20, 50, 50);
+            g.DrawEllipse(f, 700, 20, 70, 70);
 
             SolidBrush brushY = new SolidBrush(Color.Yellow);
-            g.FillEllipse(brushY, 300, 20, 50, 50);
+            g.FillEllipse(brushY, 700, 20, 70, 70);
 
 
             Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0), 3);
@@ -47,10 +57,45 @@ namespace WinForms9
             Point point1 = new Point(50, 70);
             Point point2 = new Point(120, 20);
             Point point3 = new Point(190, 70);
-            Point[] curvePoints = { point1, point2, point3 };
+            Point[] curvePoints = { point1, 
+                point2, 
+                point3 };
             e.Graphics.FillPolygon(blackBrush, curvePoints);
+            // Create pen.
+            Pen greenPen = new Pen(Color.Green, 3);
+            // Draw polygon to screen.
+            Point point4 = new Point(260, 300);
+            Point point5 = new Point(310, 140);
+            Point point6 = new Point(360, 300);
+            Point[] curvePoints2 = { point4,
+                point5,
+                point6 };
+            e.Graphics.FillPolygon(brushGreen, curvePoints2);
+            // Draw retangle to screen.
+            Pen brownPen = new Pen(Color.Brown, 3);
+            g.DrawRectangle(brownPen, 500, 400, 20, 30);
+            // Draw polygon to screen.
+            Point point7 = new Point(460, 400);
+            Point point8 = new Point(510, 240);
+            Point point9 = new Point(560, 400);
+            Point[] curvePoints3 = { point7,
+                point8,
+                point9 };
+            e.Graphics.DrawPolygon(greenPen, curvePoints3);
+
+            
+
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Text = string.Format("Координаты: {0};{1}", e.X, e.Y);
+        }
     }
     
 }
