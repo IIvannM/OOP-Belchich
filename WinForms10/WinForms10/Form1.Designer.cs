2c2613ab87d569c4297427489ff34c62d8714b8d
IIvannM <93133422+IIvannM@users.noreply.github.com>
2023-04-02 23:26:10 +0300

    Added 10 lab

---
+++ WinForms10/WinForms10/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinForms10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Multiround Pro", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(314, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinForms10/WinForms10/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Объявление переменных, кистей и массивов
+        int rotate = 0, movement = 0;
+        int xPos;
+        int y, y1;
+        bool startButton = false;
+        int j = 0;
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush lightGrayBrush = new SolidBrush(Color.LightGray);
+        SolidBrush orangeBrush = new SolidBrush(Color.OrangeRed);
+        SolidBrush groundBrush = new SolidBrush(Color.Gray);
+
+        Point[] rocketPoint = new Point[4];
+        Point[] L = new Point[7];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startButton = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Если нажата кнопка, ракета взлетает
+            if (startButton == true)
+            {
+                if (j % 2 != 0)
+                {
+                    y1 += 3;
+                    xPos -= 1;
+                }
+                else
+                    y1 -= 3;
+                j++;
+                y1 -= 3;
+                y -= 3;
+                if (j % 15 == 0)
+                    rotate += 1;
+                if (j % 30 == 0)
+                    movement += 1;
+
+            }
+            Invalidate();
+        }
+        //Присвоим значения переменным при загрузке формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            y = 350;
+            xPos = 160;
+            y1 = 386;
+        }
+        //Обозначим нажатие кнопки
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            //Если нажата кнопка, появляется пламя
+            if (startButton == true)
+            {
+                L[0] = new Point(xPos + 15, y1);
+                L[1] = new Point(xPos + 10, y1 + 25);
+                L[2] = new Point(xPos + 15, y1 + 20);
+                L[3] = new Point(xPos + 20, y1 + 35);
+                L[4] = new Point(xPos + 25, y1 + 20);
+                L[5] = new Point(xPos + 30, y1 + 25);
+                L[6] = new Point(xPos + 25, y1);
+                g.FillPolygon(orangeBrush, L);
+            }
+            //Отрисовка ракеты
+            rocketPoint[0] = new Point(xPos - rotate + 20, y);
+            rocketPoint[1] = new Point(xPos, y + 50);
+            rocketPoint[2] = new Point(xPos + 20, y + 40);
+            rocketPoint[3] = new Point(xPos + 40, y + 50);
+            g.FillRectangle(groundBrush,0,400,1000,600);
+            g.FillPolygon(lightGrayBrush, rocketPoint);
+            g.FillEllipse(blueBrush, xPos + 16 - movement, y + 15, 8, 8);
+            g.FillEllipse(blueBrush, xPos + 16 - movement, y + 25, 8, 8);
+
+        }
+
+    }
+
+}
